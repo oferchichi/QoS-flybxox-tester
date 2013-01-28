3ce5a4196dc307e1979bc8821e8a8cf3a33118dc
Omar <Omar@spaceinvader>
2013-01-28 13:48:17 +0100

    mise a jour de la charte graphic chaque 10 second

--- tn.orangetunisie.qostesterWeb/src/tn/orangetunisie/qostesterWeb/ctrl/ChartAllBean.java
+++ tn.orangetunisie.qostesterWeb/src/tn/orangetunisie/qostesterWeb/ctrl/ChartAllBean.java
@@ -1,11 +1,13 @@
 package tn.orangetunisie.qostesterWeb.ctrl;
 
+
+import java.text.SimpleDateFormat;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.naming.NamingException;
 
 import org.primefaces.model.chart.CartesianChartModel;
 import org.primefaces.model.chart.LineChartSeries;
@@ -24,60 +26,51 @@ public class ChartAllBean {
     @EJB
     FtpTestDaoLocal ftpTestDaoLocal ;
     
-    
     public ChartAllBean() {
-       createLinearModel();
+	}
     
-    }
+    @PostConstruct
+    public void init(){
+      createLinearModel();
+}
+    
+    
     private void createLinearModel() {
     	linearModel = new CartesianChartModel();
     
-			
+		
 			List<FtpTest> ftpTests = ftpTestDaoLocal.getAllTest(); 
             LineChartSeries series1 = new LineChartSeries();  
             series1.setLabel("Flybox");  
             for (FtpTest f: ftpTests){
             	if (f.getApn().equals("flybox")) {
-            		  series1.set(f.getTemps(), f.getValeur());
+            		System.out.println(f.getIdTest() + " - " + f.getApn() + " - "  +f.getValeur());
+            		String timeformat = new SimpleDateFormat("HH:mm").format(f.getTemps());
+            		series1.set(timeformat, f.getValeur());
             	}
             }
+           
+
+   
             linearModel.addSeries(series1);
-
-    
-    
-            LineChartSeries series2 = new LineChartSeries();  
-            series2.setLabel("Series 2");  
-            series2.setMarkerStyle("diamond");  
-      
-            series2.set(1, 6);  
-            series2.set(2, 3);  
-            series2.set(3, 2);  
-            series2.set(4, 7);  
-            series2.set(5, 9);  
-      
-            LineChartSeries series3 = new LineChartSeries();  
-            series3.setLabel("Series 3");
-            series3.setMarkerStyle("round");
-            
-            series3.set(0.6, 2);  
-            series3.set(2.5, 1);  
-            series3.set(3, 4);  
-            series3.set(5, 6.6);  
-            series3.set(5, 7.2);  
-
-            
-            linearModel.addSeries(series2);
-            linearModel.addSeries(series3);
-
+        
     }
 
     
     public CartesianChartModel getLinearModel() {
+    	List<FtpTest> ftpTests = ftpTestDaoLocal.getAllTest(); 
+      
+        for (FtpTest f: ftpTests){
+        	if (f.getApn().equals("flybox")) {
+        		System.out.println(f.getIdTest() + " - " + f.getApn() + " - "  +f.getValeur());
+        		String timeformat = new SimpleDateFormat("HH:mm").format(f.getTemps());
+        		linearModel.getSeries().get(0).getData().put(timeformat, f.getValeur());
+        	}
+        }
+       
 		return linearModel;
 	}
     
-    public void setLinearModel(CartesianChartModel linearModel) {
-		this.linearModel = linearModel;
-	}
+   
 
 }
